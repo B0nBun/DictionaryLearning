9a73f44b0f1dd1be96b88d9a4fb542003dccf975
B0nBun <rgusiakov@gmail.com>
2022-03-25 11:05:38 +0300

    build: electron support

---
+++ electron/main.js
@@ -0,0 +1,39 @@
+// Модули для управления приложением и создания окна
+const {app, BrowserWindow} = require('electron')
+const path = require ('path');
+const url = require ('url');
+// require('./ipcmain');
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('../build/index.html')
+
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  mainWindow.loadURL(startUrl);
+  mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
