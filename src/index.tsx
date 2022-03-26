321539d992559818f070430ddbb9402a2ce9e032
B0nBun <rgusiakov@gmail.com>
2022-03-26 21:10:44 +0300

    build: PWA support (service worker, favicons, logos and manifest.json)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+ import './reset.css';
 import './utils.css';
 import App from './Components/App/App';
 import GameStateProvider, { GameStatus } from './Providers/GameState'
@@ -31,6 +32,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
