ca5885f718d8cbd75f2cf94c9978ace218273335
B0nBun <rgusiakov@gmail.com>
2022-03-26 14:32:39 +0300

    animation: playing body animations

--- src/Components/Body/PlayingBody/PlayingBody.tsx
+++ src/Components/Body/PlayingBody/PlayingBody.tsx
@@ -4,6 +4,18 @@ import { wordStorageContext } from "../../../Providers/WordStorage"
 import { filterOutEmptyWords, last, shuffle } from "../../../utils"
 import { motion, AnimatePresence, Variants } from 'framer-motion'
 
+const animationProps = {
+    initial: {
+        opacity: 0
+    },
+    animate: {
+        opacity: 1
+    },
+    exit: {
+        opacity: 0
+    },
+}
+
 const getWordJSX = (gameState : GameState, setGameState : (gameState : GameState) => void) => {
     const handleAnswer = () => {
         setGameState({
@@ -15,14 +27,17 @@ const getWordJSX = (gameState : GameState, setGameState : (gameState : GameState
     
     if (gameState.gameMode === GameMode.DefByWord) {
         return (
-            <button className="guess-block" onClick={e => setGameState({...gameState, status : GameStatus.Definition})}>
+            <AnimatePresence exitBeforeEnter={true}>
+            <motion.button {...animationProps} key={`word-${last(gameState.playingWords)!.word}`} className="guess-block" onClick={e => setGameState({...gameState, status : GameStatus.Definition})}>
                 {last(gameState.playingWords)!.word}
-            </button>
+            </motion.button>
+            </AnimatePresence>
         )
     }
 
     return (
-        <div className="answer-block">
+        <AnimatePresence exitBeforeEnter={true}>
+        <motion.div {...animationProps} key={`word-${last(gameState.playingWords)!.word}`} className="answer-block">
             <span className="guess-result">
             {last(gameState.playingWords)!.word}
             </span>
@@ -30,7 +45,8 @@ const getWordJSX = (gameState : GameState, setGameState : (gameState : GameState
                 <button className="btn correct" onClick={handleAnswer}>Correct</button>
                 <button className="btn incorrect" onClick={handleAnswer}>Incorrect</button>
             </div>
-        </div>
+        </motion.div>
+        </AnimatePresence>
     )
 }
 
@@ -49,7 +65,8 @@ const getDefinitionJSX = (gameState : GameState, setGameState : (gameState : Gam
     
     if (gameState.gameMode === GameMode.DefByWord) {
         return (
-            <div className="answer-block">
+            <AnimatePresence exitBeforeEnter={true}>
+            <motion.div {...animationProps} key={`def-${last(gameState.playingWords)!.definition}`} className="answer-block">
                 <span className="guess-result">
                 {last(gameState.playingWords)!.definition}
                 </span>
@@ -57,14 +74,17 @@ const getDefinitionJSX = (gameState : GameState, setGameState : (gameState : Gam
                     <button className="btn correct" onClick={handleAnswer(true)}>Correct</button>
                     <button className="btn incorrect" onClick={handleAnswer(false)}>Incorrect</button>
                 </div>
-            </div>
+            </motion.div>
+            </AnimatePresence>
         )
     }
 
     return (
-        <button className="guess-block" onClick={e => setGameState({...gameState, status : GameStatus.Word})}>
+        <AnimatePresence exitBeforeEnter={true}>
+        <motion.button {...animationProps} key={`def-${last(gameState.playingWords)!.definition}`} className="guess-block" onClick={e => setGameState({...gameState, status : GameStatus.Word})}>
             {last(gameState.playingWords)!.definition}
-        </button>
+        </motion.button>
+        </AnimatePresence>
     )
 }
 
@@ -123,14 +143,20 @@ export default function PlayingBody() {
             exit="exit"
             transition={{ease: 'easeInOut'}}
         >
-            {gameState.score.total === 0 ? '' : <span className="score">{gameState.score.correct} / {gameState.score.total}</span>}
+            <span className="score">{gameState.score.correct} / {gameState.score.total}</span>
+            <AnimatePresence exitBeforeEnter={true}>
             {
-                gameState.status === GameStatus.NotPlaying ? <span className="warning">Restart to begin!</span> :
-                gameState.playingWords.length === 0 ? <span className="warning">No words left!</span> :
-                gameState.status === GameStatus.Word ? getWordJSX(gameState, setGameState) :
-                gameState.status === GameStatus.Definition ? getDefinitionJSX(gameState, setGameState) :
-                `Can't display game in status ${gameState.status}`
+                gameState.status === GameStatus.NotPlaying ?
+                    <motion.span {...animationProps} key="restart-warn" className="warning">Restart to begin!</motion.span> :
+                gameState.playingWords.length === 0 ?
+                    <motion.span {...animationProps} key="no-words-warn" className="warning">No words left!</motion.span> :
+                gameState.status === GameStatus.Word ?
+                    getWordJSX(gameState, setGameState) :
+                gameState.status === GameStatus.Definition ?
+                    getDefinitionJSX(gameState, setGameState) :
+                <motion.span {...animationProps} key="cant-display-warn">Can't display game in status ${gameState.status}</motion.span>
             }
+            </AnimatePresence>
             <div className="restart-mode-container">
                 <button className="mode-switch btn" onClick={handleModeSwitch}>Current Mode: {gameState.gameMode}</button>
                 <button className="restart btn" onClick={handleRestart}>Restart</button>
